bb0a3d3fe431b3f10faa09d16e39991a158f3a86
ChiaraRaineri <92573129+ChiaraRaineri@users.noreply.github.com>
2022-02-04 23:11:05 +0100

    Update bryce_exercise.r

--- bryce_exercise.r
+++ bryce_exercise.r
@@ -1,12 +1,12 @@
 # 1)
-
 install.packages("labdsv")
 library(labdsv)
+library(vegan)
 
 
 # 2)
+# In R studio I don't have to set the working directory
+setwd("C/bryce/data")
 
-setwd("C:/bryce/data")
-
-a <- read.csv("bryceveg.csv")
-b <- read.csv("brycesite.txt")
+bryceveg <- read.csv("bryceveg.csv")
+brycesite <- read.table("brycesite.txt", sep = " ", header = T)
