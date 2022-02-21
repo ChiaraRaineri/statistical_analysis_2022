49aa5d5bc820d3daecee2799c66b82fc9327cd0e
ChiaraRaineri <92573129+ChiaraRaineri@users.noreply.github.com>
2022-02-21 17:59:51 +0100

    Update exam_project.r

--- exam_project.r
+++ exam_project.r
@@ -1,6 +1,5 @@
 # install.packages("vegan")
 library(vegan)
-library(ggplot2)
 
 # 1)
 # Creating sub-folders
@@ -28,6 +27,7 @@ ncol(BCI)  # Number of species
 nrow(BCI_env)  # Number of observations
 ncol(BCI_env)  # Number of environmental variables
 
+# Matrices are better than data frames when the number of species is very high
 
 # 3)
 ?BCI
@@ -42,21 +42,22 @@ str(BCI)  # Community matrix
 str(BCI_env)  # Data frame
 # The elements that are missing from BCI_env are the names of the columns
 # I solved it in the process of importing data by adding sep
+# Talk about data types
 
-# Convert characters into factors
+# Convert characters into factors, because they can be categorical (factors are not just atomic vectors, they are objects)
 # I should order Age.cat
 # unique factors within a vector
 unique(BCI_env$Age.cat)  # I have only c2 and c3
-factor(BCI_env$Age.cat, levels = c("c2", "c3"), ordered = T)
-factor(BCI_env$Habitat, levels = c("OldSlope", "OldLow", "Swamp", "OldHigh", "Young"))
-factor(BCI_env$Stream, levels = c("Yes", "No"))
+BCI_env$Age.cat <- factor(BCI_env$Age.cat, levels = c("c2", "c3"), ordered = T)
+BCI_env$Habitat <- factor(BCI_env$Habitat, levels = c("OldSlope", "OldLow", "Swamp", "OldHigh", "Young"))
+BCI_env$Stream <- factor(BCI_env$Stream, levels = c("Yes", "No"))
 
 
 # 4)
 # Let's do summary statistics
 # I notice there are some missing values and some NAs
-summary(BCI)
 summary(BCI_env)
+# For the factors the numbers are the counts for each category
 
 
 # I should check if there are NAs
@@ -88,8 +89,8 @@ BCI_env <- read.table("data/BCI_env.txt",
 
 # 5) 
 # plot the univariate distribution (so the distribution of just one variable)
-# You can use ggplot2
 # we have numerical variables and categorical variables
+# I have to use the function table to make a contingency table that contains the sum of the observations for each category
 # Distribution of habitat types:
 png("outputs/Figure1.png", res = 300, width = 3000, heigh = 2000)
 barplot(table(BCI_env$Habitat),
@@ -132,13 +133,11 @@ summary(BCI_env$sr)
 
 
 # 7c) 
-png("outputs/Figure3.png", res = 300, width = 3000, heigh = 2000)
 boxplot(BCI_env$sr ~ BCI_env$Habitat,
         main = "Distribution of species richness in respect with Habitat",
         ylim = c(70, 110),
         ylab = "Species richness",
         xlab = "Habitat type")
-dev.off()
 # Young and swamp are almost symmetrical because they have few observations
 # The group with the highest variance is OldLow (larger max and min)
 
@@ -146,12 +145,11 @@ dev.off()
 # 8)
 # Species richness with respect to EnvHet
 # Two numerical variables (scatterplot)
-png("outputs/Figure4.png", res = 300, width = 3000, heigh = 2000)
 plot(BCI_env$EnvHet, BCI_env$sr,
      xlab = "Environmental heterogeneity", 
      ylab = "Species richness")
-     abline(lm$coefficients[1], lm$coefficients[2], col = "red")
-dev.off()
+abline(lm(BCI_env$sr ~ BCI_env$EnvHet, data = BCI_env),
+       col = "red")
 
 # Pearson's correlation test
 cor.test(BCI_env$EnvHet, BCI_env$sr)  
@@ -167,4 +165,174 @@ summary(lm)
 # Are the residuals normally distributed?
 
 
+
+
+
+
+
+# CODE ON R STUDIO
+
+# Statistical analysis R project
+
+
+# Task 1
+# Creating sub-folders
+dir.create(path = "outputs")
+dir.create(path = "data")
+
+# Installing the package if required and recalling the library
+# install.packages(vegan)
+library(vegan)
+
+
+# Task 2
+# Importing the datasets
+BCI <- read.csv("data/BCI.csv")
+BCI_env <- read.table("data/BCI_env.txt", 
+                      sep = " ", 
+                      header = T,
+                      na.strings = c("NA", ""))
+
+# sep = " " is used to correct the shifted values
+# header = T is used to transform the first row into column titles
+# na.strings = c("NA", "") is used to transform blank spaces into NAs
+
+# Counting the number of observations, species and environmental variables
+nrow(BCI)      # Number of observations = number of rows (50)
+ncol(BCI)      # Number of species = number of columns in the community matrix (225)
+ncol(BCI_env)  # Number of environmental variables = number of columns in the data frame (9)
+
+
+# Task 3
+# Checking the documentation
+?BCI
+
+# Checking the structure of the dataset
+str(BCI)  
+# data.frame of	50 observations of  225 variables
+# All the variables are integer
+str(BCI_env)
+# data.frame of	50 observations of  9 variables
+# There are variables that are integer, numeric and charachters
+
+
+# Transforming columns to match types and structures described in the documentation
+# Checking Age.cat levels
+unique(BCI_env$Age.cat)
+# c3 is older than c2
+# Transforming Age.cat into an ordered factor
+BCI_env$Age.cat <- factor(BCI_env$Age.cat, 
+       levels = c("c2", "c3"), 
+       ordered = T)
+
+# Transforming Stream into a factor (not ordered)
+BCI_env$Stream <- factor(BCI_env$Stream, 
+       levels = c("Yes", "No"))
+
+# Transforming Habitat into a factor (not ordered)
+BCI_env$Habitat <- factor(BCI_env$Habitat, 
+       levels = c("OldSlope", "OldLow", "Swamp", "OldHigh", "Young"))
+
+# Transforming Geology into a factor (not ordered)
+BCI_env$Geology <- factor(BCI_env$Geology, 
+                          levels = c("Tb"))
+
+
+# Task 4
+# Calculating summary statistics
+summary(BCI_env)
+
+
+# Checking if there are any NAs
+# NAs can be removed with the na.omit function
+index_na <- which(is.na(BCI_env), arr.ind = T)
+index_na  # Position of the NAs (row and column)
+index_na <- index_na[, 1]  # Subsetting the first column (row)
+
+# Removing NA values
+BCI_env <- na.omit(BCI_env)
+nrow(BCI_env)  # Three rows were removed
+nrow(BCI)  # But in BCI there are still those three rows
+# Omitting the corresponding rows in BCI
+BCI <- BCI[- index_na, ]
+
+# Now the number of rows is equal to 47
+
+# Exporting the datasets with NAs removed
+write.table(x = BCI_env, "outputs/BCI_env_modified.txt")
+write.table(x = BCI, "outputs/BCI_modified.txt")
+
+
+# Task 5
+# Graphically exploring the univariate distribution of environmental variables 
+# Plotting a histogram for the numerical variable EnvHet
+hist(BCI_env$EnvHet,
+     main = "Relative Environmental Heterogeneity distribution",
+     xlab = "Environmental Heterogeneity")
+
+# Plotting and exporting a bar chart for the distribution of Habitat types
+png("outputs/Figure1.png", res = 300, width = 3000, heigh = 2000)
+barplot(table(BCI_env$Habitat),
+        main = "Types of Habitat",
+        ylim = c(0, 26),
+        ylab = "Frequency",
+        xlab = "Habitat type")
+dev.off()
+
+# Plotting and exporting a stacked bar chart for the distribution of streams among the different habitats
+png("outputs/Figure2.png", res = 300, width = 3000, heigh = 2000)
+barplot(table(BCI_env$Stream, BCI_env$Habitat),
+        main = "Presence or absence of Stream",
+        ylim = c(0, 26),
+        ylab = "Frequency",
+        xlab = "Habitat type",
+        legend.text = T)
+dev.off()
+
+
+# Task 6
+# Converting the community matrix into presence/absence using decostand function (vegan package)
+BCI <- decostand(BCI, method = "pa")
+
+# Exporting presence/absence matrix in .csv format
+write.csv(x = BCI, file = "outputs/BCI_pa.csv")
+
+
+# Task 7
+# Calculating species richness
+# Also adding it to the environmental variables dataframe as a new column
+BCI_env$sr <- specnumber(BCI)
+
+# Summary statistics for species richness
+summary(BCI_env$sr)
+
+# How does species richness distribute in respect with Habitat variable? 
+# Making a boxplot 
+boxplot(BCI_env$sr ~ BCI_env$Habitat,
+        main = "Distribution of species richness in respect with Habitat",
+        ylim = c(70, 110),
+        ylab = "Species richness",
+        xlab = "Habitat type")
+
+# Task 8
+# Plotting the distribution of species richness in respect with the available numeric environmental variables
+# Plotting Species richness with respect to EnvHet (two numerical variables = scatterplot)
+plot(BCI_env$EnvHet, BCI_env$sr,
+     xlab = "Environmental heterogeneity", 
+     ylab = "Species richness")
+abline(lm(BCI_env$sr ~ BCI_env$EnvHet, data = BCI_env),
+       col = "red")  # Regression line
+
+# Pearson's correlation test
+cor.test(BCI_env$EnvHet, BCI_env$sr)
+# null hypothesis: correlation equal to 0
+# alternative hypothesis: true correlation is not equal to 0
+# p-value = 0.968 -> non significant -> null hypothesis can't be rejected
+# cor = -0.006018848 -> not significant correlation between sr and EnvHet 
+
+# Regression model
+lm <- lm(sr ~ EnvHet, data = BCI_env)
+summary(lm)
+# Adjusted R-squared = -0.02219 -> poor fit of the model
+# p-value = 0.968 -> very high value -> the mode is not significant
 
